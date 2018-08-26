fac900b138bbec8491309382b5bf59d064f83f4e
eric <eric.y.corsican@gmail.com>
2018-08-26 13:14:11 +0800

    fix build

--- lwip/src/custom/sys_arch.c
+++ lwip/src/custom/sys_arch.c
@@ -6,22 +6,14 @@
 #elif _WIN32
    //define something for Windows (32-bit)
 #elif __APPLE__
-    #include "TargetConditionals.h"
-    #if TARGET_OS_IPHONE && TARGET_IPHONE_SIMULATOR
-        // define something for simulator
-    #elif TARGET_OS_IPHONE
-        // define something for iphone
-    #else
-        #define TARGET_OS_OSX 1
-        #include <mach/mach_time.h>
-        u32_t sys_now(void) {
-            uint64_t now = mach_absolute_time();
-            mach_timebase_info_data_t info;
-            mach_timebase_info(&info);
-            now = now * info.numer / info.denom / NSEC_PER_MSEC;
-            return (u32_t)(now);
-        }
-    #endif
+    #include <mach/mach_time.h>
+    u32_t sys_now(void) {
+        uint64_t now = mach_absolute_time();
+        mach_timebase_info_data_t info;
+        mach_timebase_info(&info);
+        now = now * info.numer / info.denom / NSEC_PER_MSEC;
+        return (u32_t)(now);
+    }
 #elif __linux
     #include <sys/time.h>
     u32_t sys_now(void)
