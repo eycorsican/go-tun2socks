bec05e6adfc6c350cc2fe0ed89bc07c1d40a1767
eric <eric.y.corsican@gmail.com>
2018-10-25 23:22:30 +0800

    AddRoute impl for windows

--- route/route_windows.go
+++ route/route_windows.go
@@ -2,8 +2,17 @@ package route
 
 import (
 	"errors"
+	"fmt"
+	"os/exec"
 )
 
 func AddRoute(dest, netmask, gateway string) error {
-	return errors.New("not implemented")
+	out, err := exec.Command("route", "add", dest, "mask", netmask, gateway, "metric", "5").Output()
+	if err != nil {
+		if len(out) != 0 {
+			return errors.New(fmt.Sprintf("%v, output: %s", err, out))
+		}
+		return err
+	}
+	return nil
 }
