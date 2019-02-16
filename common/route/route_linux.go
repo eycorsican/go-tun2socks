fcb34a76cdba1502671e498f33bcff32782ebad5
eric <eric.y.corsican@gmail.com>
2019-02-16 18:55:14 +0800

    support dynamic routing on linux

--- common/route/route_linux.go
+++ common/route/route_linux.go
@@ -2,8 +2,18 @@ package route
 
 import (
 	"errors"
+	"fmt"
+	"os/exec"
 )
 
 func AddRoute(dest, netmask, gateway string) error {
 	return errors.New("not implemented")
+	out, err := exec.Command("ip", "route", "add", dest+"/32", "via", gateway).Output()
+	if err != nil {
+		if len(out) != 0 {
+			return errors.New(fmt.Sprintf("%v, output: %s", err, out))
+		}
+		return err
+	}
+	return nil
 }
