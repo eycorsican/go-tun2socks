dbdc60ddb2b9b3165c1c49b2f7b402fdf172864d
eric <eric.y.corsican@gmail.com>
2019-06-21 01:05:54 +0800

    refactor

--- core/output_export.go
+++ core/output_export.go
@@ -14,13 +14,14 @@ func output(p *C.struct_pbuf) C.err_t {
 	// In most case, all data are in the same pbuf struct, data copying can be avoid by
 	// backing Go slice with C array. Buf if there are multiple pbuf structs holding the
 	// data, we must copy data for sending them in one pass.
+	totlen := int(p.tot_len)
 	if p.tot_len == p.len {
-		buf := (*[1 << 30]byte)(unsafe.Pointer(p.payload))[:int(p.len):int(p.len)]
-		OutputFn(buf)
+		buf := (*[1 << 30]byte)(unsafe.Pointer(p.payload))[:totlen:totlen]
+		OutputFn(buf[:totlen])
 	} else {
-		buf := NewBytes(int(p.tot_len))
+		buf := NewBytes(totlen)
 		C.pbuf_copy_partial(p, unsafe.Pointer(&buf[0]), p.tot_len, 0) // data copy here!
-		OutputFn(buf[:int(p.tot_len)])
+		OutputFn(buf[:totlen])
 		FreeBytes(buf)
 	}
 	return C.ERR_OK
