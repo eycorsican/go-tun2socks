bd1dac481acbdfe83e89a0d2d9d5d48c5844a41d
Jason <xjasonlyu@gmail.com>
2019-07-02 13:33:24 +0800

    update fakedns interface

--- common/dns/dns.go
+++ common/dns/dns.go
@@ -17,10 +17,10 @@ type DnsCache interface {
 
 type FakeDns interface {
 	// GenerateFakeResponse generates a fake dns response for the specify request.
-	GenerateFakeResponse(request []byte) ([]byte, error)
+	// GenerateFakeResponse(request []byte) ([]byte, error)
 
-	// QueryDomain returns the corresponding domain for the given IP.
-	QueryDomain(ip net.IP) string
+	// IPToHost returns the corresponding domain for the given IP.
+	IPToHost(ip net.IP) string
 
 	// IsFakeIP checks if the given ip is a fake IP.
 	IsFakeIP(ip net.IP) bool
