f5d54085d27f6e12cae4310ad2e6721a22e106d8
Ox Cart <ox.to.a.cart@gmail.com>
2020-11-09 21:07:08 -0600

    Memory reduction

--- core/buffer_pool.go
+++ core/buffer_pool.go
@@ -1,35 +1,13 @@
 package core
 
 import (
-	"sync"
+	pool "github.com/libp2p/go-buffer-pool"
 )
 
-var pool *sync.Pool
-
-const BufSize = 2 * 1024
-
-func SetBufferPool(p *sync.Pool) {
-	pool = p
-}
-
 func NewBytes(size int) []byte {
-	if size <= BufSize {
-		return pool.Get().([]byte)
-	} else {
-		return make([]byte, size)
-	}
+	return pool.Get(size)
 }
 
 func FreeBytes(b []byte) {
-	if len(b) >= BufSize {
-		pool.Put(b)
-	}
-}
-
-func init() {
-	SetBufferPool(&sync.Pool{
-		New: func() interface{} {
-			return make([]byte, BufSize)
-		},
-	})
+	pool.Put(b)
 }
